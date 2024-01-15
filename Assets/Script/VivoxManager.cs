9fd855fb60f9db025fb836a1f5576060e5e3e9c6
Avinash Pandey <avinash.pdy@gmail.com>
2024-01-15 22:56:59 +0530

    Added permission flow

--- Assets/Script/VivoxManager.cs
+++ Assets/Script/VivoxManager.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Vivox;
-using UnityEditor;
 using UnityEngine.Android;
 using UnityEngine.UI;
 
@@ -13,6 +14,7 @@ public class VivoxManager : MonoBehaviour
 {
     [SerializeField] private string UserDisplayName = "User";
     [SerializeField] private Text _statusText;
+    [SerializeField] private TextMeshProUGUI _logText;
     
     // Speaker toggle buttons
     [SerializeField] private Toggle _speakToAllChannelToggle;
@@ -27,14 +29,35 @@ public class VivoxManager : MonoBehaviour
     private const string AllChannelName = "all";
     private const string TeamChannelName = "team";
     
+    private const int MinVolume = -50;
+    private const int MaxVolume = 40;
+    
     private List<VivoxParticipant> _allChannelParticipants = new List<VivoxParticipant>();
     
+    private Action<string> _permissionCallback;
+    
     // Start is called before the first frame update
-    async void Start()
+    void Start()
     {
-        RequestMicrophonePermission();
-        
         ToggleButtons(false);
+        
+        RequestMicrophonePermission(permission =>
+        {
+            SetupVoiceChatAsync();
+        });
+    }
+    
+    private void OnDestroy()
+    {
+        VivoxService.Instance.LoggedIn -= VivoxServiceOnLoggedIn;
+        VivoxService.Instance.LoggedOut -= VivoxServiceOnLoggedOut;
+        
+        VivoxService.Instance.ParticipantAddedToChannel -= VivoxServiceOnParticipantAddedToChannel;
+        VivoxService.Instance.ParticipantRemovedFromChannel -= VivoxServiceOnParticipantRemovedFromChannel;
+    }
+    
+    private async void SetupVoiceChatAsync()
+    {
         await InitializeAsync();
         
         SetupVivoxEvents();
@@ -44,15 +67,6 @@ public class VivoxManager : MonoBehaviour
         await SetupAllChannels();
         
         ToggleButtons(true);
-    }
-
-    private void OnDestroy()
-    {
-        VivoxService.Instance.LoggedIn -= VivoxServiceOnLoggedIn;
-        VivoxService.Instance.LoggedOut -= VivoxServiceOnLoggedOut;
-        
-        VivoxService.Instance.ParticipantAddedToChannel -= VivoxServiceOnParticipantAddedToChannel;
-        VivoxService.Instance.ParticipantRemovedFromChannel -= VivoxServiceOnParticipantRemovedFromChannel;
     }
 
     async Task InitializeAsync()
@@ -104,14 +118,14 @@ public class VivoxManager : MonoBehaviour
 
     private async Task SetupAllChannels()
     {
-        // Should we await this?
+        // Should we await these?
         SetTransmissionMode(TransmissionMode.None, null);
         
         Debug.Log($"Joining all channel");
-        await JoinGroupChannelAsync(AllChannelName);
+        JoinGroupChannelAsync(AllChannelName);
         
         Debug.Log($"Joining Team channel");
-        await JoinGroupChannelAsync(TeamChannelName);
+        JoinGroupChannelAsync(TeamChannelName);
     }
     
     private async Task JoinGroupChannelAsync(string channelName)
@@ -124,16 +138,16 @@ public class VivoxManager : MonoBehaviour
         //ChannelOptions options = new ChannelOptions();
         //options.MakeActiveChannelUponJoining = false;
         
-        // #if UNITY_EDITOR
-        // await VivoxService.Instance.JoinEchoChannelAsync(channelName, ChatCapability.AudioOnly, null);
-        // #else
+        #if UNITY_EDITOR
+        await VivoxService.Instance.JoinEchoChannelAsync(channelName, ChatCapability.AudioOnly, null);
+        #else
         await VivoxService.Instance.JoinGroupChannelAsync(channelName, ChatCapability.AudioOnly, null);
-        //#endif
+        #endif
         timeToJoinChannel = Time.time - timeToJoinChannel;
         _statusText.text = $"Joined {channelName} in {timeToJoinChannel:0.00}s";
         
         // Keep the channel volume to almost none till player taps on speaker button
-        SetChannelVolume(channelName, -50);
+        SetChannelVolume(channelName, MinVolume);
     }
 
     #endregion
@@ -145,8 +159,8 @@ public class VivoxManager : MonoBehaviour
         if(VivoxService.Instance == null || VivoxService.Instance.IsLoggedIn == false)
             return;
         
-        SetChannelVolume(AllChannelName,  50);
-        SetChannelVolume(TeamChannelName,  50);
+        SetChannelVolume(AllChannelName,  MaxVolume);
+        SetChannelVolume(TeamChannelName,  MaxVolume);
     }
     
     public void ListenToTeamButtonTap()
@@ -154,8 +168,8 @@ public class VivoxManager : MonoBehaviour
         if(VivoxService.Instance == null || VivoxService.Instance.IsLoggedIn == false)
             return;
         
-        SetChannelVolume(AllChannelName,  -50);
-        SetChannelVolume(TeamChannelName,  50);
+        SetChannelVolume(AllChannelName,  MinVolume);
+        SetChannelVolume(TeamChannelName,  MaxVolume);
     }
 
     public void MuteSpeakerButtonTap()
@@ -166,8 +180,8 @@ public class VivoxManager : MonoBehaviour
         if (_muteSpeakerToggle.isOn)
         {
             Debug.Log($"Mute speaker");
-            SetChannelVolume(AllChannelName, -50);
-            SetChannelVolume(TeamChannelName, -50);
+            SetChannelVolume(AllChannelName, MinVolume);
+            SetChannelVolume(TeamChannelName, MinVolume);
         }
     }
     
@@ -272,27 +286,57 @@ public class VivoxManager : MonoBehaviour
         if(VivoxService.Instance == null || VivoxService.Instance.IsLoggedIn == false)
             return;
         
-        Debug.Log($"Active channels: {VivoxService.Instance.ActiveChannels.Count}");
+        StringBuilder logString = new StringBuilder();
+        
+        logString.AppendLine($"Active channels: {VivoxService.Instance.ActiveChannels.Count}");
         foreach (var channel in VivoxService.Instance.ActiveChannels)
         {
-            Debug.Log($"Channel: {channel.Key} - Volume {channel.Value.Count}");
+            logString.AppendLine($"Channel: {channel.Key} - Volume {channel.Value.Count}");
         }
         
-        Debug.Log($"Transmitting channels: {VivoxService.Instance.TransmittingChannels.Count}");
+        logString.AppendLine($"Transmitting channels: {VivoxService.Instance.TransmittingChannels.Count}");
         foreach(var channel in VivoxService.Instance.TransmittingChannels)
         {
-            Debug.Log($"Channel: {channel}");
+            logString.AppendLine($"Channel: {channel}");
         }
+        
+        Debug.Log(logString.ToString());
+        _logText.SetText(logString.ToString());
     }
     
-    private bool RequestMicrophonePermission()
+    private void RequestMicrophonePermission(Action<string> callback)
     {
-        if(Permission.HasUserAuthorizedPermission(Permission.Microphone))
-            return false;
+        if (Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            _permissionCallback = null;
+            callback?.Invoke(Permission.Microphone);
+            return;
+        }
         
-            // We do not have permission to use the microphone.
-            // Ask for permission or proceed without the functionality enabled.
-            Permission.RequestUserPermission(Permission.Microphone);
-            return true;
+        _permissionCallback = callback;
+
+        var callbacks = new PermissionCallbacks();
+        callbacks.PermissionDenied += PermissionCallbacks_PermissionDenied;
+        callbacks.PermissionGranted += PermissionCallbacks_PermissionGranted;
+        callbacks.PermissionDeniedAndDontAskAgain += PermissionCallbacks_PermissionDeniedAndDontAskAgain;
+        Permission.RequestUserPermission(Permission.Microphone, callbacks);
+    }
+
+    private void PermissionCallbacks_PermissionDeniedAndDontAskAgain(string obj)
+    {
+        _logText.SetText($"Permission denied and don't ask again: {obj}");
+        _permissionCallback?.Invoke(obj);
+    }
+
+    private void PermissionCallbacks_PermissionGranted(string obj)
+    {
+        _logText.SetText($"Permission granted: {obj}");
+        _permissionCallback?.Invoke(obj);
+    }
+
+    private void PermissionCallbacks_PermissionDenied(string obj)
+    {
+        _logText.SetText($"Permission denied: {obj}");
+        _permissionCallback?.Invoke(obj);
     }
 }
